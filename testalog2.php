df78b6c53148d2ce724eb95e3664b03cd1f7a1a3
Joris <joris.bertier@gmail.com>
2023-03-01 16:08:56 +0100

    tout les tests CN sont réussi

--- testalog2.php
+++ testalog2.php
@@ -5,13 +5,17 @@ CN2 Si note comprise entre 12 et 14
 CN3 Si note comprise entre 14 et 16
 CN4 Si note comprise entre 16 et 18
 CN5 Si note superieure à 18
+CN6 Si note comprise entre 0 et 12
 */
 
 function mention($note) {
     if ($note < 0 || $note > 20 ) {
     return false; 
     }
-    
+
+    elseif (0 <= $note && $note < 12) {
+        return "Note de merde";
+    }
     elseif (12 <= $note && $note < 14) {
         return "AB";
     }
@@ -35,13 +39,73 @@ function mention($note) {
 /*var_dump(mention(21));*/
 
 /*CN1*/
-function testMention() {
+function testMention1() {
 if(mention(21)== false) {
-    echo "TEST testMention : Reussi !";
+    echo "TEST testMention1 : Reussi !\n";
 }
 else {
- echo "Test testMention : Echec";
+ echo "Test testMention1 : Echec";
 }
 }
 
-testMention();
+testMention1();
+
+/*CN2*/
+function testMention2() {
+    if(mention(13)== "AB") {
+        echo "TEST testMention2 : Reussi !\n";
+    }
+    else {
+     echo "Test testMention2 : Echec";
+    }
+    }
+    
+    testMention2();
+
+    /*CN3*/
+    function testMention3() {
+    if(mention(15)== "Bien") {
+        echo "TEST testMention3 : Reussi !\n";
+    }
+    else {
+     echo "Test testMention3 : Echec";
+    }
+    }
+    
+    testMention3();
+
+    /*CN4*/
+    function testMention4() {
+    if(mention(17)== "TB") {
+        echo "TEST testMention4 : Reussi !\n";
+    }
+    else {
+     echo "Test testMention4 : Echec";
+    }
+    }
+    
+    testMention4();
+
+    /*CN5*/
+    function testMention5() {
+    if(mention(19)== "Excellent") {
+        echo "TEST testMention5 : Reussi !\n";
+    }
+    else {
+     echo "Test testMention5 : Echec";
+    }
+    }
+    
+    testMention5();
+
+        /*CN6*/
+        function testMention6() {
+            if(mention(8)== "Note de merde") {
+                echo "TEST testMention6 : Reussi !\n";
+            }
+            else {
+             echo "Test testMention6 : Echec";
+            }
+            }
+            
+            testMention6();
