a5dfffbbe2480d1e3ad2e912fe805151878e6edf
Joris <joris.bertier@gmail.com>
2023-03-01 15:44:23 +0100

    Test CN1

--- testalog2.php
+++ testalog2.php
@@ -1,5 +1,11 @@
 
 <?php
+/*CN1 Si inferieure à 0 et supérieur à 20
+CN2 Si note comprise entre 12 et 14
+CN3 Si note comprise entre 14 et 16
+CN4 Si note comprise entre 16 et 18
+CN5 Si note superieure à 18
+*/
 
 function mention($note) {
     if ($note < 0 || $note > 20 ) {
@@ -26,4 +32,16 @@ function mention($note) {
     
 }
 
-var_dump(mention(21));
+/*var_dump(mention(21));*/
+
+/*CN1*/
+function testMention() {
+if(mention(21)== false) {
+    echo "TEST testMention : Reussi !";
+}
+else {
+ echo "Test testMention : Echec";
+}
+}
+
+testMention();
