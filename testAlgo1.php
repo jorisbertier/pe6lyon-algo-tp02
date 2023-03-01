41c445717ea3c9f26fe8e0e665c8ef59ae7e4885
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-03-01 14:21:21 +0100

    écriture du test testMean_N1

--- testAlgo1.php
+++ testAlgo1.php
@@ -5,4 +5,16 @@ function mean($nb1, $nb2) {
 }
 
 echo "Test moyenne: ";
-var_dump(mean(10, 20));
+var_dump(mean(10, 20)); 
+
+function testmean_2() 
+ {
+    if (mean(10,20)==15) {
+        echo "test mean(2): ok" ; 
+    }
+    else { echo "test mean (2) : echec" ;
+    }
+ }
+
+testmean_2();
+
