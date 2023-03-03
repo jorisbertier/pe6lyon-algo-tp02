ab13f2e8cbedda95c64009915d9629413d1db3fb
Joris <joris.bertier@gmail.com>
2023-03-03 14:43:52 +0100

    Exercice 3 réussi

---
+++ soutienAlgo3.1.php
@@ -0,0 +1,6 @@
+Écrivez un programme qui permet de jouer au nombre mystère avec l'utilisateur.
+Le programme choisit un nombre mystère aléatoirement entre 1 et 100, puis il demande à l'utilisateur de deviner ce nombre.
+Si le nombre choisi par l'utilisateur est trop petit, on affiche "trop petit"; s'il est trop grand, on affiche "trop grand".
+Si l'utilisateur a trouvé le bon nombre, on lui affiche "gagné" et le nombre de coups qu'il a joués.
+L'ordinateur dispose d'une fonction Lire() qui récupère une valeur saisie au clavier par l'utilisateur.
+Par exemple:       saisie <- Lire()          va enregistrer dans la variable "saisie" ce que l'utilisateur vient de taper.
